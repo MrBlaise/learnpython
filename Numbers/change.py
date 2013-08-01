59c4da200477bd58d7b348e04e2b3a03e3e9df61
MrBlaise <rostas.balazs@gmail.com>
2013-08-01 14:04:49 +0100

    Added change.py - basic program not yet finished

---
+++ Numbers/change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Change Calculator
+# This is the basic of the program
+# Later it will be more user-friendly
+# And will give more accurate
+# answers using cents as well
+
+def changeCalc(cost, money):
+
+	if money < cost:
+
+		print('Please pay ', cost - money,'$ more', sep='')
+
+	else:
+
+		return -1*(cost - money)
+
+def main():
+
+	cost = int(input('Enter the cost of the item: '))
+	money = int(input('Enter the money given: '))
+
+	change = changeCalc(cost,money)
+
+	if type(change) == int:
+		print('The change is: ',change, '$', sep='')
+
+if __name__ == '__main__':
+	main()
