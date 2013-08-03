3e7ec665b3100f155cb910b19269f435d97c57ff
MrBlaise <rostas.balazs@gmail.com>
2013-08-03 12:51:57 +0100

    isPrime function modified - more efficient code

--- Numbers/prime.py
+++ Numbers/prime.py
@@ -19,7 +19,10 @@ def isPrime(x):
     if x == 2:
         return True
 
-    for i in range(2, x):
+    if x % 2 == 0:
+        return False
+
+    for i in range(3, int(x ** 0.5) + 1, 2):
         if x % i == 0:
             return False
 
