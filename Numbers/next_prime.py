8d0c3c97b9483720f13022ed07bb470415a35626
Saurabh Gharat <37373648+saurabhgharat@users.noreply.github.com>
2018-03-14 22:51:00 +0530

    Update next-prime.py
    even more simplified code

--- Numbers/next_prime.py
+++ Numbers/next_prime.py
@@ -1,61 +1,9 @@
-#!/usr/bin/env python3
-
-# Next Prime Number
-# Generate prime numbers until
-# the user chooses to stop
-
-
-def isPrime(x):
-    """
-    Checks whether the given
-    number x is prime or not
-    """
-
-    if x == 2:
-        return True
-
-    if x % 2 == 0:
-        return False
-
-    for i in range(3, int(x**0.5)+1, 2):
-        if x % i == 0:
-            return False
-
-    return True
-
-
-def genPrime(currentPrime):
-    """
-    Returns the next prime
-    after the currentPrime
-    """
-
-    newPrime = currentPrime + 1
-
-    while True:
-
-        if not isPrime(newPrime):
-            newPrime += 1
+n= int(input("enter a number")) 
+if n>1:
+        for i in range(2,n):
+            
+            if n%i==0:
+                print("{} is not a prime number".format(n))
+                n= int(input("enter a number"))
         else:
-            break
-
-    return newPrime
-
-
-def main():  # Wrapper function
-
-    currentPrime = 2
-
-    while True:
-
-        answer = input('Would you like to see the next prime? (Y/N) ')
-
-        if answer.lower().startswith('y'):
-            print(currentPrime)
-            currentPrime = genPrime(currentPrime)
-
-        else:
-            break
-
-if __name__ == '__main__':
-    main()
+            print("{} is a prime number".format(n))
